8f44aa6c35a92790dcc725f1a93fb6fecd7395ea
donny41 <dyuandyu@gmail.com>
2024-06-10 22:48:51 +0900

    laravel-basic-kadai提出

--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/posts', function () {
+//     return view('posts/index');
+// });
+
+Route::get('/posts', [PostController::class, 'index']);
