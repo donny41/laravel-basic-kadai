8ff0d587247da9093a45f56c9ff8f3b24a0d273b
donny41 <dyuandyu@gmail.com>
2024-06-11 10:29:13 +0900

    Eloquent ORMを使ってデータを取得しようの課題提出

--- laravel-basic-kadai/app/Http/Controllers/PostController.php
+++ laravel-basic-kadai/app/Http/Controllers/PostController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use App\Models\Post;
 
 class PostController extends Controller
 {
@@ -13,4 +15,12 @@ class PostController extends Controller
 
         return view('posts.index', compact('posts'));
     }
+
+    public function show($id){
+        $post = Post::find($id);
+        return view('posts.show', compact('post'));
+        // $product = Product::find($id);
+        // return view('products.show', compact('product'));
+    }
+
 }
---
+++ laravel-basic-kadai/app/Models/Post.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+}
+
+// class Product extends Model
+// {
+//     use HasFactory;
+
+//      // 1つの商品は1つの仕入先に属する
+//      public function vendor() {
+//         return $this->belongsTo(Vendor::class, 'vendor_code', 'vendor_code');
+//     }    
+    
+// }
